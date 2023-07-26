61ae5dbca81ddb16a89857017bd043e339b9db2d
Bastiaan Smis <bastiaan.smis@net-it.be>
2023-07-26 13:00:54 +0200

    5. Add TestClass to see history on classes and methods

---
+++ TestApp/TestApp/TestClass.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestClass
+    {
+        public string MyContent { get; set; }
+
+        public TestClass()
+        {
+            MyContent = string.Empty;
+        }
+    }
+}
